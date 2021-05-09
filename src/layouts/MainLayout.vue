6e4d90f97c370c86dec486da2a6c823415063611
Paulo Tiago <paulotiagocm@hotmail.com>
2021-05-09 12:05:24 -0300

    first page timer

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,83 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Quasar Timer
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-12"
+        >
+          Workout
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Home',
+    caption: 'home',
+    icon: 'home',
+    link: '/#/'
+  },
+  {
+    title: 'Timer',
+    caption: 'timer',
+    icon: 'rss_feed',
+    link: '/#/timer'
+  },
+  {
+    title: 'About',
+    caption: 'See this',
+    icon: 'favorite',
+    link: '/#/about'
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Timer.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-toolbar class="bg-blue text-white shadow-2 rounded-borders flex-center">
+      <div class="text-h6">Timer</div>
+    </q-toolbar>
+    <q-card class="my-card">
+      <q-card-section>
+        <div class="text-h3">{{timer}}</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions vertical>
+        <q-btn flat class="bg-green">Start</q-btn>
+        <q-btn flat class="bg-red">Stop</q-btn>
+      </q-card-actions>
+    </q-card>
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn
+        color="secondary"
+        @click="$q.fullscreen.toggle()"
+        :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
+        :label="$q.fullscreen.isActive ? 'Exit Fullscreen' : 'Go Fullscreen'"
+      />
+    </q-page-sticky>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Timer',
+  data: () => {
+    return {
+      timer:'02:00'
+    }
+  }
+}
+</script>
