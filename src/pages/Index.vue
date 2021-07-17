2228c7305b55290369c42bb46e602ebc9e2102f5
Paulo Tiago <paulotiagocm@hotmail.com>
2021-07-17 11:01:02 -0300

    feat: fills to config workout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,31 @@
 <template>
   <q-page class="flex flex-center">
-    <a class="flex flex-center h1" href="#/timer">
-      <q-icon size="xl" name="play_circle_outline"></q-icon>
-      <h3>Go to workout!</h3>
-    </a>
+    <div class="q-gutter-y-md column" style="max-width: 500px">
+      <q-input filled stack-label label="sets" name="sets" id="sets" v-model="sets" bg-color="light-green" />
+      <q-input filled stack-label label="work" name="work" id="work" v-model="work" bg-color="light-blue" />
+      <q-input filled stack-label label="rest" name="rest" id="rest" v-model="rest" bg-color="yellow" />
+      <q-btn color="deep-orange" @click="goWorkout">
+        <q-icon size="xl" name="play_circle_outline"></q-icon>
+        <h3>Go!</h3>
+      </q-btn>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  methods: {
+    goWorkout() {
+      this.$router.push(`timer?sets=${this.sets}&work=${this.work}&rest=${this.rest}`);
+    }
+  },
+  data: () => {
+    return {
+      sets: 6,
+      work: 30,
+      rest: 30
+    }
+  }
 }
 </script>
