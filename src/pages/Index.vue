3ebc8029238f5adba15ec50e41c7b825e2ef5db8
Paulo Tiago <paulotiagocm@hotmail.com>
2021-07-17 11:04:16 -0300

    chore: type number to inputs

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,12 @@
 <template>
   <q-page class="flex flex-center">
     <div class="q-gutter-y-md column" style="max-width: 500px">
-      <q-input filled stack-label label="sets" name="sets" id="sets" v-model="sets" bg-color="light-green" />
-      <q-input filled stack-label label="work" name="work" id="work" v-model="work" bg-color="light-blue" />
-      <q-input filled stack-label label="rest" name="rest" id="rest" v-model="rest" bg-color="yellow" />
+      <q-input filled stack-label label="sets" name="sets" id="sets" 
+        type="number" v-model="sets" bg-color="light-green" />
+      <q-input filled stack-label label="work" name="work" id="work" 
+        type="number" v-model="work" bg-color="light-blue" />
+      <q-input filled stack-label label="rest" name="rest" id="rest" 
+        type="number" v-model="rest" bg-color="yellow" />
       <q-btn color="deep-orange" @click="goWorkout">
         <q-icon size="xl" name="play_circle_outline"></q-icon>
         <h3>Go!</h3>
